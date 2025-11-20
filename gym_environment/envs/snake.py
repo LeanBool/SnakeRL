82d4ea7216ae6f495e7d60f0f539f2986f6795cb
LeanBool <alexgraf44@gmail.com>
2025-11-20 17:45:20 +0100

    Added reset implementation

--- gym_environment/envs/snake.py
+++ gym_environment/envs/snake.py
@@ -1,5 +1,5 @@
 import sys, os
-sys.stdout = open(os.devnull,'w') # suppress pygame import warning
+sys.stdout = open(os.devnull,'w') # suppress pygame import warning about deprecated dependency
 sys.stderr = open(os.devnull,'w')
 import pygame
 sys.stdout = sys.__stdout__
@@ -20,6 +20,7 @@ class SnakeEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
     def __init__(self, render_mode=None, window_size=None, grid_size=None):
+        self.window = None
         self.grid_size = (6, 4) if not grid_size else grid_size
         self.render_mode = render_mode
         self.window_size = (800, 600) if not window_size else window_size
@@ -29,10 +30,10 @@ class SnakeEnv(gym.Env):
 
         self.action_space = spaces.Discrete(4)
         self._action_to_direction = {
-            Actions.RIGHT.value: np.array([1, 0]),
-            Actions.UP.value: np.array([0, 1]),
-            Actions.LEFT.value: np.array([-1, 0]),
-            Actions.DOWN.value: np.array([0, -1]),
+            Actions.RIGHT: np.array([1, 0]),
+            Actions.UP: np.array([0, 1]),
+            Actions.LEFT: np.array([-1, 0]),
+            Actions.DOWN: np.array([0, -1]),
         }
 
         self.observation_space = spaces.Dict(
@@ -42,13 +43,38 @@ class SnakeEnv(gym.Env):
             }
         )
 
-        self.window = None
+        self.grid = np.zeros(self.grid_size, dtype=int)
+        self.v_tiles = np.zeros((*self.grid_size, 2), dtype=int)
 
-    def reset(self):
-        pass
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+
+        self._agent_location = (self.grid_size[0]//2, self.grid_size[1]//2) # spawn in center
+
+        self._target_location = self._agent_location
+        while np.array_equal(self._target_location, self._agent_location):
+            self._target_location = self.np_random.integers(0, self.grid_size, size=2, dtype=int)
+
+        self.v_tiles[self._agent_location] = self._action_to_direction[Actions.UP]
+
+        observation = self._get_obs()
+        info = self._get_info()
+
+        if self.render_mode == "human":
+            self._render_frame()
+
+        return observation, info
 
     def step(self):
         pass
+
+    def _get_obs(self):
+        return {"agent": self._agent_location, "target": self._target_location}
+
+    def _get_info(self):
+        return {
+            "distance": np.linalg.norm(self._agent_location - self._target_location, ord=1) # manhattan dist
+        }
 
     def render(self):
         if self.render_mode == "rgb_array":
