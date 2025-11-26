f0b9254ff0e84b473441faaa420006cb0fff5cc9
LeanBool <alexgraf44@gmail.com>
2025-11-26 20:57:40 +0100

    .

--- runner.py
+++ runner.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     grid_size = (6, 5)
     window_size = (800, 600)
     testing_episode_count = int(1e4)
-    training_timesteps = int(1)
+    training_timesteps = int(1e6)
     n_envs = 8
     window_size = (window_size[0] // int(np.sqrt(n_envs)), window_size[1] // int(np.sqrt(n_envs)))
 
